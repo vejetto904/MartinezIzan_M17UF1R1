5fefc1793639b1026656fb93946ac08d1de910b0
vejetto904 <83954743+vejetto904@users.noreply.github.com>
2023-10-23 17:59:46 +0200

    miniom y nivel 3

---
+++ GravityChange/Assets/RobotMalo.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+
+public class MovimientoEnemigo : MonoBehaviour
+{
+    public float puntoInicioX;
+    public float puntoFinalX;
+    public float velocidad = 5.0f;
+    public float tiempoEsperaEnPuntoFinal = 1.0f;
+
+    private Vector3 puntoInicio;
+    private Vector3 puntoFinal;
+    private bool mirandoDerecha = true;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        puntoInicio = new Vector3(puntoInicioX, transform.position.y, transform.position.z);
+        puntoFinal = new Vector3(puntoFinalX, transform.position.y, transform.position.z);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        StartCoroutine(MoverEnemigo());
+    }
+
+    IEnumerator MoverEnemigo()
+    {
+        while (true)
+        {
+            // Mover hacia el punto final
+            while (Mathf.Abs(transform.position.x - puntoFinal.x) > 0.1f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, puntoFinal, velocidad * Time.deltaTime);
+                MirarDireccion(puntoFinal);
+                yield return null;
+            }
+
+            // Esperar en el punto final
+            yield return new WaitForSeconds(tiempoEsperaEnPuntoFinal);
+
+            // Mover hacia el punto de inicio
+            while (Mathf.Abs(transform.position.x - puntoInicio.x) > 0.1f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, puntoInicio, velocidad * Time.deltaTime);
+                MirarDireccion(puntoInicio);
+                yield return null;
+            }
+        }
+    }
+
+    void MirarDireccion(Vector3 targetPosition)
+    {
+        if (targetPosition.x > transform.position.x && !mirandoDerecha)
+        {
+            // Mover a la derecha
+            mirandoDerecha = true;
+            spriteRenderer.flipX = false;
+        }
+        else if (targetPosition.x < transform.position.x && mirandoDerecha)
+        {
+            // Mover a la izquierda
+            mirandoDerecha = false;
+            spriteRenderer.flipX = true;
+        }
+    }
+}
